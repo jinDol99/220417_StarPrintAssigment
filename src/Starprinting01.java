f61a1a34c452639166906b584ef37f1ecebbc1c0
parkm <parkm@LenovoY540-5578>
2022-04-14 17:55:51 +0900

    1차 초안 - 220414_1755

--- src/Starprinting01.java
+++ src/Starprinting01.java
@@ -7,7 +7,16 @@ public class Starprinting01 {
 		int a = sc.nextInt();
 		
 		for (int i = 1; i <=(a*2-1); i++) {
-			System.out.print("*");
+			if (i <= a) {
+				for (int j = 1; j <= i; j++)
+					System.out.printf("*");
+			}
+			else {
+				for (int j = ((a*2)-i); j > 0; j--)
+					System.out.printf("*");
+			}	
+			System.out.printf("%n");
 		}
+		
 	}
 }
