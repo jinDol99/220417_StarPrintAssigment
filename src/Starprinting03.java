9403bb57c7f8b73c1a0a21de611235d41cdeaa14
parkm <parkm@LenovoY540-5578>
2022-04-16 12:15:02 +0900

    초안 커밋 - 220416_1214

---
+++ src/Starprinting03.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Starprinting03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("삼각형의 높이 입력: ");
+		int triangleHeight = sc.nextInt();
+		
+		
+	}
+
+}
