9e802efebb6627e5cf885d562d02bab06350d39f
parkm <parkm@LenovoY540-5578>
2022-04-16 11:48:59 +0900

    초안01 작성중 - 220416_1148

---
+++ src/Starprinting02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Starprinting02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("옆으로 누운 삼각형의 높이 입력: ");
+		int triangleHeight = sc.nextInt();
+		
+		
+	}
+}
